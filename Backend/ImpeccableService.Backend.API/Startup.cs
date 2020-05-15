38770b09d5f51acc46af5de1006af6114b6483a5
Aleksandar Kahriman <aleksandar.kahriman@gmail.com>
2020-05-15 11:20:11 +0200

    Demo

--- Backend/ImpeccableService.Backend.API/Startup.cs
+++ Backend/ImpeccableService.Backend.API/Startup.cs
@@ -86,7 +86,7 @@ namespace ImpeccableService.Backend.API
         }
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ISecurityEnvironmentVariables securityEnvironmentVariables)
         {
             _securityEnvironmentVariables = securityEnvironmentVariables;
