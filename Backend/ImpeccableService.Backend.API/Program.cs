f92641b122167a263b439710ef78055c041330ec
Aleksandar Kahriman <aleksandar.kahriman@gmail.com>
2020-06-05 17:43:49 +0200

    Env variables wip

--- Backend/ImpeccableService.Backend.API/Program.cs
+++ Backend/ImpeccableService.Backend.API/Program.cs
@@ -30,7 +30,7 @@ namespace ImpeccableService.Backend.API
                         var projectId = config.Build().GetSection("ProjectId").Value;
                         config.AddSecretsManager(configurator: options =>
                         {
-                            options.SecretFilter = entry => entry.Name.Contains(config.Build().GetSection("ProjectId").Value);
+                            options.SecretFilter = entry => entry.Name.StartsWith(projectId);
                             options.KeyGenerator = (entry, key) => key.Split("---").Last();
                         });
                     }
